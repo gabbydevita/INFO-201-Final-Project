6348de1d75153743ad20b62e408082b618f895f5
CeciliaMeza2004 <ameza2@uw.edu>
2023-11-14 15:05:39 -0800

    added comment

--- datawrangling.R
+++ datawrangling.R
@@ -5,3 +5,4 @@ kc_2020_df <- read.csv("King_County_Census_2020_blocks_with_PL_94-171_Redistrict
 census_2010_df <- read.csv("2010_Census_Block_Seattle_-_Population_Statistics.csv") 
 
 clean_kc_df <- filter(kc_2020_df, Place == "Seattle") 
+# something crazy
